46e4462bb3933c8cdd1a5ab31f55dfdd0ec63cf8
mokny <33606734+mokny@users.noreply.github.com>
2024-02-09 13:52:31 +0100

    User Class

--- lib/websocketserver.py
+++ lib/websocketserver.py
@@ -12,6 +12,27 @@ import vars as v
 def msg(method, payload):
     return json.dumps({"m": method, "p": payload})
 
+
+class User():
+    def __init__(self, websocket):
+        self.id = str(uuid.uuid4())
+        self.websocket = websocket
+        self.create()
+
+    def create(self):
+        v.users[self.id] = self
+        v.connections.add(self.websocket)
+
+    def remove(self):
+        del v.users[self.id]
+        v.connections.remove(self.websocket)
+
+    async def send(self, method, payload):
+        await self.websocket.send(msg(method, payload))
+
+
+
+
 class server(threading.Thread):
     def __init__(self, host, port):  
         print("Initializing WS Server") 
@@ -19,25 +40,27 @@ class server(threading.Thread):
         self.host = host
         self.port = port
 
+
+    # Send to single client
+    async def sendToClient(self, websocket, method, payload):
+        await websocket.send(msg(method, payload))
+    
+    # Broadcast to all clients
+    async def broadcast(self, method, payload):
+        websockets.broadcast(v.connections, msg(method, payload))
+
     async def connectionhandler(self, websocket):
         connectionid = str(uuid.uuid4())
 
         try:
             # Register user
-            v.users[connectionid] = {
-                'connectionid': connectionid,
-                'ip': websocket.remote_address[0],
-                'socket': websocket,
-                'data': {},
-            }
-
-            v.connections.add(websocket)
+            user = User(websocket)
 
             # Broadcast the amount of connected users to all clients
-            websockets.broadcast(v.connections, msg('ONLINEUSERS', len(v.connections)))
+            await self.broadcast('ONLINEUSERS', len(v.connections))
             
             # Require the new connection to authenticate
-            await websocket.send(msg('AUTH', False))
+            await user.send('AUTH', False)
 
             # Parse messages from client
             async for raw in websocket:
@@ -46,17 +69,16 @@ class server(threading.Thread):
                 payload = message['p']
 
                 if method == 'AUTH':
-                    await websocket.send(msg('AUTH', True))
+                    await user.send('AUTH', True)
                 else:
-                    await websocket.send(msg('ERR', {'code': 1, 'desc': 'Unknown method'}))
-                    websockets.broadcast(v.connections, msg('NEW', False))
-                    
+                    await user.send('ERR', {'code': 1, 'desc': 'Unknown method'})
+                    await self.broadcast('Something', payload)
+
         finally:
             # Unregister user
-            del v.users[connectionid]
-            v.connections.remove(websocket)
-            websockets.broadcast(msg('DISC', False))
-            #websockets.broadcast(USERS, users_event())
+            user.remove()
+            await self.broadcast('ONLINEUSERS', len(v.connections))
+            
 
     async def startserver(self):
         async with websockets.serve(self.connectionhandler, self.host, self.port):
