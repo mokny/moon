d2f1a88bb1d3d8d6996fe5be228ac01100f22cd1
mokny <33606734+mokny@users.noreply.github.com>
2024-02-19 09:45:52 +0100

    predict hostname

--- lib/main.py
+++ lib/main.py
@@ -1,4 +1,5 @@
 import mokkalib
+import socket
 
 class Main():
     def __init__(self, database, webserver, websocketserver):  
@@ -7,7 +8,10 @@ class Main():
         self.websocketserver = websocketserver
 
         # Set Script variables for the webserver.
-        self.webserver.setscriptvar('websocket.host', self.websocketserver.host)
+        hostname = self.websocketserver.host
+        if hostname == '0.0.0.0': hostname = socket.gethostname()
+        self.webserver.setscriptvar('websocket.host', hostname)
+
         self.webserver.setscriptvar('websocket.port', self.websocketserver.port)
 
         
