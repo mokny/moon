5910fe93337173cf6bd87e02c0b7dc88a3bfbf15
mokny <33606734+mokny@users.noreply.github.com>
2024-02-20 09:02:27 +0100

    install

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+
+echo " ______                    _ "
+echo "|  ___ \                  | |"
+echo "| | _ | | ___   ___  ____ | |"
+echo "| || || |/ _ \ / _ \|  _ \|_|"
+echo "| || || | |_| | |_| | | | |_ "
+echo "|_||_||_|\___/ \___/|_| |_|_|"
+
+echo "Website: https://github.com/mokny/moon"
+
+
+read -p "Enter Workspace-Ident: " workspace
 UUID=$(cat /proc/sys/kernel/random/uuid)
 tmppath="/tmp/moon_${UUID}"
 git clone https://github.com/mokny/tanks "${tmppath}"
-mokka workspace create moon
-mokka install moon ${tmppath}
+mokka workspace create ${workspace}
+mokka install ${workspace} ${tmppath}
 rm -rf ${tmppath}
